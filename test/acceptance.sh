7e1496e84dfad885097d805a1f5d351e0bfdaf3e
Kai Davenport <kaiyadavenport@gmail.com>
2015-05-13 16:27:02 +0100

    acceptance test will wait for tasks to have started before trying to contact them

--- test/acceptance.sh
+++ test/acceptance.sh
@@ -14,32 +14,37 @@ function launch-mongo() {
 function launch-app() {
   local id=$1;
   
-  cat example/todomvc/db.json | \
+  cat example/todomvc/app.json | \
   sed "s/\"id\":\"app\"/\"id\":\"app-$id\"/" | \
   curl -i -H 'Content-type: application/json' -d @- http://172.16.255.250:8080/v2/apps
 }
+
+function is-task-running() {
+  curl -sS http://172.16.255.250:8080/v2/apps/$1 | jq .app.tasksRunning
+}
+
+function wait-for-task() {
+  local running=$(is-task-running $1)
+  while [[ "$running" == "null" ]]
+  do
+    echo "wait for task $1" && sleep 1
+    running=$(is-task-running $1)
+  done
+  echo "task $1 is running"
+  sleep 5
+}
+
 
 function run-test() {
   local unixsecs=$(date +%s)
   local flockervolumename="testflocker$unixsecs"
 
-  launch-mongo $unixsecs spinning
-  launch-app $unixsecs
+  #launch-mongo $unixsecs spinning
+  wait-for-task mongo
+  #launch-app $unixsecs
+  wait-for-task app
+
+  echo "apps are running!"
 }
 
-# we write the datestring into the guestbook with no spaces because URL encoding
-writedate=`echo "$datestring" | sed 's/ //g'`
-
-echo "writing data to node1 ($datestring)"
-vagrant ssh node1 -c "sudo docker run --rm -v /flocker/$flockervolumename:/data busybox sh -c \"echo $datestring > /data/file.txt\""
-echo "reading data from node2"
-filecontent=`vagrant ssh node2 -c "sudo docker run --rm -v /flocker/$flockervolumename:/data busybox sh -c \"cat /data/file.txt\""`
-if [[ $filecontent == *"$datestring"* ]]
-then
-  echo "Datestring: $datestring found!"
-else
-  do-error "The contents of the text file is not $datestring it is: $filecontent"
-fi
-
-echo "all tests were succesful"
-exit 0
+run-test
